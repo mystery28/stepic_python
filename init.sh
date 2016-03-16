70c00ce02d4f51fa340e7ab8672197a00644d74a
Mystery28 <vladimir.axt@gmail.com>
2016-03-16 13:26:03 +0200

    Change script init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 ﻿
 # copy to destination folder
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 
 # restart services
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/gunicorn restart

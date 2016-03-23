43d05739e6892114ac62d5496468c814f07c9129
Mystery28 <vladimir.axt@gmail.com>
2016-03-23 23:09:37 +0200

    Change conf

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 ﻿
 # copy to destination folder
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 
 # restart services
-gunicorn -b 0.0.0.0:8080 hello:app &
+#gunicorn -b 0.0.0.0:8080 hello:app &
 sudo /etc/init.d/nginx restart
-#sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/gunicorn restart
